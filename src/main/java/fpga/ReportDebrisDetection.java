455dc76921eba5f626a080c74897763805578b2b
mittens <matinsdavid@gmail..com>
2017-03-04 14:34:25 -0700

    Created bare-bones for the ReportDebrisDetection Pipeline Node

---
+++ src/main/java/fpga/ReportDebrisDetection.java
@@ -0,0 +1,22 @@
+package fpga;
+
+import fpga.objectdetection.Debris;
+
+/**
+ * Created by David M on 3/4/17.
+ *
+ * Report Debris Detection
+ * [FROM DR. ROMAN]
+ * “RDD gets the extraction result from Extract and writes the description of the detected debris,
+ * a success flag, and a completion flag that indicates is done.
+ * When no debris is found, the success flag is set to false and the completion flag is set to true.
+ * The PC Board needs to reset the flags and the debris data.”
+ *
+ */
+public class ReportDebrisDetection
+{
+  public static void reportDetectedDebris(Debris[] test) {
+    // TODO: implement requirements specified above.
+  }
+
+}
