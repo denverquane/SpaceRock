fa913f378c8d5954f6b5079605d106d576f1551a
mittens <matinsdavid@gmail..com>
2017-03-04 15:13:05 -0700

    Created a bare-bones class for Sending a successful frame

---
+++ src/main/java/fpga/SendSuccessfulFrame.java
@@ -0,0 +1,20 @@
+package fpga;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * Created by David M on 3/4/17.
+ *
+ * Send Successful Frame
+ * [FROM DR. ROMAN]
+ * “Extract also passes a success or failure signal regarding the detection of a debris to Send Successful Frame.
+ * If success is received, the original frame received from Retrieve Image is written to the output.”
+ *
+ */
+public class SendSuccessfulFrame
+{
+  public static void sendFrame(BufferedImage frame) {
+    // TODO: implement requirements specified above.
+  }
+
+}
