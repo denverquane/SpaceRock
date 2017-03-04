190e3d6d3b5083f90df5b56b1413c6554c797848
DebRez <rezanka@comcast.net>
2017-03-04 09:22:03 -0700

    Outline for Operator, Camera and Scheduler.
    Signed-off-by: DebRez <rezanka@comcast.net>

---
+++ src/main/java/debrisProcessingSubsystem/Scheduler.java
@@ -0,0 +1,26 @@
+package debrisProcessingSubsystem;
+
+/**
+ * This will be the Scheduler object shown
+ * in the SADD. The Scheduler will interface with the DebrisCollection, Operator,
+ * and Camera objects.
+ * This is a preliminary placeholder and very subject to change.
+ * Created by dsr on 3/4/17.
+ */
+public class Scheduler //implements DebrisCollection, Camera, Operator
+{
+  
+  public void check_Collection()
+  {
+
+  }
+  public void check_Operator()
+  {
+
+  }
+  public void check_Camera()
+  {
+
+  }
+
+}
