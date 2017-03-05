1daad3463a21d5c24997979cf14c65162bdbb913
justindonthomas <justin.don.thomas@gmail.com>
2017-03-05 07:22:23 -0700

    DebrisList interface for Debris List objects.

---
+++ src/main/java/debrisProcessingSubsystem/debrisCollection/DebrisList.java
@@ -0,0 +1,19 @@
+package debrisProcessingSubsystem.debrisCollection;
+
+/**
+ * This is the interface for the debris list objects under the DebrisCollection
+ * object in the SADD.
+ * Created by jdt on 3/5/17.
+ */
+public interface DebrisList {
+    /**
+     * This method will be used to get a debris element from the list.
+     */
+    //Debris getDebrisElement();
+
+    /**
+     * This method will be used to tell if the list is empty.
+     * @return True if empty.
+     */
+    boolean isEmpty();
+}
