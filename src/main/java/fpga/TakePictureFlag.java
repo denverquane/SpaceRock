4d9a8fc0ee588effcbbcee7ac05209ba293bd8cb
Michael Handrock <mjhandrock@gmail.com>
2017-03-04 18:18:06 -0700

    Updated TakePictureFlag with my own TakePictureThread code, and added a setter function.

--- src/main/java/fpga/TakePictureFlag.java
+++ src/main/java/fpga/TakePictureFlag.java
@@ -1,15 +1,32 @@
 package fpga;
 
+import sensor.SensorInterface;
 /**
  * Created by Ken Kressin on 4/3/17. Description:
  */
 public class TakePictureFlag implements Runnable {
+  private SensorInterface si;
 
   public TakePictureFlag(String name){
-
+    si = null;
   }
   @Override
   public void run() {
+    /*TODO: set up loop in run()*/
+    /*while (ready register is false) {}*/
+    /*ready = false;*/
+    si.takePicture();
+    /*while (ReadAck == false) {}*/
+    /*TODO: find and call the appropriate ReadAck function*/
+    /*ready = true;*/
+  }
 
+  /**
+   * SET SENSOR
+   * Choose which sensor to send instructions to,
+   * to avoid problems with the constructor.
+   */
+  public void setSensor(SensorInterface sensor){
+    si = sensor;
   }
 }
