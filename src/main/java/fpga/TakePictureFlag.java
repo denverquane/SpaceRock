55c194c50e726263a22a8bf44b90666bf79471bb
Robert <roberttrujillo1982@gmail.com>
2017-03-04 23:28:56 -0700

    Implemented what Ken had in comments of his skeleton.

--- src/main/java/fpga/TakePictureFlag.java
+++ src/main/java/fpga/TakePictureFlag.java
@@ -1,24 +1,81 @@
 package fpga;
-
 import sensor.SensorInterface;
+import java.util.List;
+
 /**
  * Created by Ken Kressin on 4/3/17. Description:
  */
 public class TakePictureFlag implements Runnable {
   private SensorInterface si;
+  private MemoryMap mm;
+
+
+  public class MemoryMap{
+    public List<Reg> Registers;
+  }
+
+  public class Reg{
+
+    public String name;
+    public Boolean status;
+    public String  someData;
+
+  }
+
+  public Boolean Ready(String RegName){
+
+    Boolean RegStatus = false;
+
+    for(Reg r : this.mm.Registers) {
+      if(r.name == RegName) {
+        RegStatus = r.status;
+      }
+    }
+
+    return RegStatus;
+  }
+
+  public void SetRegNotReady(String RegName){
+
+    for(Reg r : this.mm.Registers) {
+      if(r.name == RegName) {
+        r.status = false;
+      }
+    }
+  }
+
+  public void Read_Acknowledge(){
+
+    while(!si.ready()){
+
+    }
+    si.takePicture();
+
+    /*Wait for image to be processed*/
+    while(!si.imageReady()){
+    }
+  }
 
   public TakePictureFlag(String name){
     si = null;
   }
   @Override
   public void run() {
+
     /*TODO: set up loop in run()*/
     /*while (ready register is false) {}*/
+    while(!Ready("TakePicReg")){
+
+    }
+
     /*ready = false;*/
-    si.takePicture();
+    SetRegNotReady("TakePicReg");
+
     /*while (ReadAck == false) {}*/
     /*TODO: find and call the appropriate ReadAck function*/
     /*ready = true;*/
+    Read_Acknowledge();
+
   }
 
   /**
@@ -29,4 +86,8 @@ public class TakePictureFlag implements Runnable {
   public void setSensor(SensorInterface sensor){
     si = sensor;
   }
+
+
+
+
 }
