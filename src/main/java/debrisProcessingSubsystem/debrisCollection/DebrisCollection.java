8a9e007262022dd9588b610a247f738df27d14ff
justindonthomas <justin.don.thomas@gmail.com>
2017-03-05 08:34:46 -0700

    Comments

--- src/main/java/debrisProcessingSubsystem/debrisCollection/DebrisCollection.java
+++ src/main/java/debrisProcessingSubsystem/debrisCollection/DebrisCollection.java
@@ -3,6 +3,9 @@ package debrisProcessingSubsystem.debrisCollection;
 /**
  * This will be the interface implemented by the Debris Collection object shown
  * in the SADD.
+ * This interface should contain all outward facing methods for the
+ * Debris collection data structure.
+ *
  * This is a preliminary placeholder and very subject to change.
  * Created by jdt on 3/4/17.
  */
--- src/main/java/debrisProcessingSubsystem/debrisCollection/DebrisList.java
+++ src/main/java/debrisProcessingSubsystem/debrisCollection/DebrisList.java
@@ -3,17 +3,18 @@ package debrisProcessingSubsystem.debrisCollection;
 /**
  * This is the interface for the debris list objects under the DebrisCollection
  * object in the SADD.
+ * This interface should contain all outward facing methods for the debris lists.
  * Created by jdt on 3/5/17.
  */
 public interface DebrisList {
     /**
      * This method will be used to get a debris element from the list.
      */
-    DebrisRecord getDebrisElement();
+    public DebrisRecord getDebrisElement();
 
     /**
      * This method will be used to tell if the list is empty.
      * @return True if empty.
      */
-    boolean isEmpty();
+    public boolean isEmpty();
 }
