82122f42d274befd2ee10b71d39400d3a77f9b98
Ken Kressin <jemezgeek.gmail.com>
2017-03-06 23:14:21 -0700

    Added TODO and general comments in the run() method on general way to code this.

--- src/main/java/fpga/GetFrameFlag.java
+++ src/main/java/fpga/GetFrameFlag.java
@@ -24,7 +24,15 @@ framethread = new Thread(this, name);
       /**
        * TODO:  Add the method to tell the sensor to send the requested frame.
        */
-
+/*
+   Poll the control register for a 'getFrame' request.  This request should come with new
+   X and Y coordinates for the center of the frame, and a frame size.
+   If new request, set our ready() to false, then send the new data to the sensor.
+   Look for some sort of ack from the sensor - not sure if this should be the 'imageReady()'
+   or the 'sensor.ready()' signal.  Might have to ask the sensor team for a new ack for a
+   GetFrame request - something on the lines of 'frameSent()'
+   Once we get confirmation, set our ready() to true.
+ */
 
 
     }
