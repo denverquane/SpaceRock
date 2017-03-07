b612478796551daeb86d4ee984b25760b93f9105
James Holland <jamesfholland@gmail.com>
2017-03-06 19:58:03 -0700

    Reorganize to remove global static members in objectdetection

--- src/main/java/fpga/Extract.java
+++ src/main/java/fpga/Extract.java
@@ -9,6 +9,12 @@ import java.util.List;
  * Created by jholland13 on 3/6/17.
  */
 public class Extract {
+
+  /**
+   * Extract a list of Debris objects from the image
+   * @param image true for debris, false for none
+   * @return List of debris with x,y centers and diameter.
+   */
   static List<Debris> extract(boolean[][] image) {
     List<Debris> debrisList = DebrisScanner.searchDebrisMap(image);
     DebrisScanner.printDebris(debrisList);
