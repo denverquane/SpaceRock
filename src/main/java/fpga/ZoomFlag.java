9fac4ab7cb972a1caf150ed11670365d1069c120
Ken Kressin <jemezgeek.gmail.com>
2017-03-06 22:10:07 -0700

    Added a shutdown method, so we can cleanly quit the thread.

--- src/main/java/fpga/ZoomFlag.java
+++ src/main/java/fpga/ZoomFlag.java
@@ -4,13 +4,24 @@ import sensor.ZoomLevel;
  * Created by Ken Kressin on 4/3/17. Description:
  */
 public class ZoomFlag implements Runnable {
+Thread zoomFlag;
+  boolean running = true;
 
+  public void shutdown(){
+    running = false;
+  }
 
   public ZoomFlag(String name){
+    zoomFlag = new Thread(this, name);
 
   }
   @Override
   public void run() {
+    while(running){
+
+
+
+    }
 
   }
 }
