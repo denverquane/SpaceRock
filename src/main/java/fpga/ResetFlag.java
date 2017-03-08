22afbf61e01a9d3bc7dd97db488214a923d49515
Ultimate-Ediri <eigharoro@unm.edu>
2017-03-07 21:44:30 -0700

    Simple test/prototype for the ResetFlag

--- src/main/java/fpga/ResetFlag.java
+++ src/main/java/fpga/ResetFlag.java
@@ -3,35 +3,55 @@ package fpga;
 /**
  * Created by Ken Kressin on 4/3/17. Description:
  */
-public class ResetFlag implements Runnable {
-Thread resetflag;
+public class ResetFlag implements Runnable
+{
+  Thread resetflag;
   boolean running = true;
   //boolean reset = false;
 
-public ResetFlag(String name){
-resetflag = new Thread(this, name);
-}
+  /*TODO: Delete this as soon as the actual register exists.*/
+  private DummyRegister CR;
+
+  public ResetFlag(String name)
+  {
+    resetflag = new Thread(this, name);
+  }
 
   /**
    * Method to shut the thread down
    */
-  public void shutdown(){
+  public void shutdown()
+  {
     running = false;
   }
 
+  /*TODO: Delete DummyRegister as soon as the actual register exists.*/
+  private class DummyRegister{
+    public boolean resetting;
+    public boolean ready;
+  }
+
   @Override
-  public void run() {
+  public void run()
+  {
     while(running){
-/**
- * TODO:  We need the control register name to call for the reset command.
- */
-    /*
+
+      /**
+       * TODO:  We need the control register name to call for the reset command.
+       */
+
+      /*
        poll the control register looking for a signal indicating we are resetting the
        sensor - possibly a boolean?
        When true, we set our ready() to false, then send the reset signal to the sensor.
        When the sensor replies with a sensor ready, then we set our ready() method to true.
-     */
+       */
 
+      if(CR.resetting)
+      {
+        CR.ready = false;
+        FlagController.sensor.reset();
+      }
     }
 
   }
