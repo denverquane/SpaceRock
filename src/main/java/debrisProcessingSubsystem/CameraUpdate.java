e723d8a4c7487aa76a3163309fd794e3da032b01
Nick Spurlock <Nick@Maxam-PC>
2017-03-12 10:35:26 -0600

    Update Objects
    -Added the top level update object which contains update type access.
    -Added child update objects that should be defined by the teams working
    	on those objects (operator, camera, debris collector).
    -Added the and enum to track the update type if we need more
    	functionality than instanceOf can offer

---
+++ src/main/java/debrisProcessingSubsystem/CameraUpdate.java
@@ -0,0 +1,17 @@
+package debrisProcessingSubsystem;
+
+/**
+ * The update specifically for the Camera component which contains information
+ * the camera object needs to update.
+ * @author Nicholas Spurlok
+ *
+ */
+public class CameraUpdate extends ComponentUpdate
+{
+
+  public CameraUpdate(UpdateType updateType)
+  {
+    super(updateType);
+  }
+
+}
---
+++ src/main/java/debrisProcessingSubsystem/DebrisCollectorUpdate.java
@@ -0,0 +1,19 @@
+package debrisProcessingSubsystem;
+
+/**
+ * The update specifically for the Debris Collector component which contains information
+ * the debris collector object needs to update.
+ * @author Nicholas Spurlok
+ *
+ */
+public class DebrisCollectorUpdate extends ComponentUpdate
+{
+  public DebrisCollectorUpdate(UpdateType updateType)
+  {
+    super(updateType);
+  }
+}
+
+
+
+
