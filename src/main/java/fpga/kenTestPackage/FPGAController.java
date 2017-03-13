fc6daab75fdbe5524f5837207ccee73c87870357
Ken Kressin <jemezgeek.gmail.com>
2017-03-13 17:40:47 -0600

    Created new FPGA Controller which should replace FlagController.  FPGAController will create all the FPGA flags from one master FPGAThread class.

---
+++ src/main/java/fpga/kenTestPackage/FPGAController.java
@@ -0,0 +1,56 @@
+package fpga.kenTestPackage;
+
+import fpga.memory.MemoryMap;
+import sensor.SensorSimulation;
+
+/**
+ * Created by Ken Kressin on 13/3/17. Description:
+ * This is the master controller class, used to start the FPGA flag threads, and shut them down.
+ * FPGAController calls FPGAThread, passing the flag name from the Enum 'FPGAFlags' to start the
+ * correct thread.
+ *
+ * When needed, call FPGAController.shutdown() to kill the FPGA flags in an orderly fashion.
+ */
+public class FPGAController {
+
+  /*
+    The sensor and memory map objects will be instantiated as static objects in FPGAThread, so that
+    the threads are all using the same object.
+   */
+  //static SensorSimulation sensor = new SensorSimulation();
+  //static MemoryMap memMap = new MemoryMap();
+
+  public FPGAController() {
+    System.out.println("Starting Control Register flags...");
+
+  }
+  // Stsart all the flags.
+
+  FPGAThread zoom = new FPGAThread("zoomThread", FPGAFlags.ZOOM);
+  FPGAThread onOff = new FPGAThread("onOff", FPGAFlags.ON_OFF);
+  FPGAThread reset = new FPGAThread("reset", FPGAFlags.RESET);
+  FPGAThread takeImage = new FPGAThread("takeImage", FPGAFlags.TAKE_IMAGE);
+  FPGAThread setGetFrame = new FPGAThread("setGetFrame", FPGAFlags.SET_GET_FRAME);
+
+  /**
+   * Basic try/catch block to shut all the flags down in an orderly fashion.
+   */
+  public void shutDown() {
+    zoom.shutdown();
+    onOff.shutdown();
+    reset.shutdown();
+    takeImage.shutdown();
+    setGetFrame.shutdown();
+    try{
+      zoom.fpgaThread.join();
+      onOff.fpgaThread.join();
+      reset.fpgaThread.join();
+      takeImage.fpgaThread.join();
+      setGetFrame.fpgaThread.join();
+
+      System.out.println("Flags successfully shut down...");
+    }catch(InterruptedException e){
+      e.printStackTrace();
+    }
+  }
+}
