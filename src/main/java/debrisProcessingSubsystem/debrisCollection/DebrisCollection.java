ca16d9a24da5a5017efe132d16676651810b11bc
justindonthomas <justin.don.thomas@gmail.com>
2017-03-13 09:07:19 -0600

    Added outgoingUpdate list, and hooked it up to pollComponent method.

--- src/main/java/debrisProcessingSubsystem/debrisCollection/DebrisCollection.java
+++ src/main/java/debrisProcessingSubsystem/debrisCollection/DebrisCollection.java
@@ -3,12 +3,20 @@ package debrisProcessingSubsystem.debrisCollection;
 import debrisProcessingSubsystem.Updatable;
 import debrisProcessingSubsystem.Update;
 
+import java.util.LinkedList;
+
 /**
+ * DebrisCollection manages the debris list on the CPU side of the satellite.
+ * It receives updates from the scheduler and performs the appropriate actions
+ * and responses.
+ * It manages the lists, returns debris for sending to earth, and attempts to
+ * track debris over time.
  * Created by jdt on 3/13/17.
  */
 public class DebrisCollection implements Updatable {
 
     private DebrisList newDebris, oldDebris;
+    private LinkedList<Update> outgoingUpdates;
 
 
     /**
@@ -17,26 +25,35 @@ public class DebrisCollection implements Updatable {
     public DebrisCollection(){
         newDebris = null;
         oldDebris = null;
+        outgoingUpdates = new LinkedList<>();
     }
 
     /**
      * Call this to send an update to this component.
-     * Returns a response Update
+     * Returns a response Update.
      * TODO write this.
      * @param theUpdate The update to be received by this component.
      * @return A response update.
      */
     public Update updateComponent(Update theUpdate){
+        //Determine what kind of message we have.
+        //perform appropriate action
+        //return response if appropriate.
         return null;
     }
 
     /**
      * Return any available updates.
-     * TODO write this.
-     * @return An update is ready.
+     * @return If an outgoing update is available, return it, else return null.
      */
     public Update pollComponent(){
-        return null;
+        Update retUpdate;
+        if(outgoingUpdates.isEmpty()){
+            retUpdate = null;
+        } else {
+            retUpdate = outgoingUpdates.removeFirst();
+        }
+        return retUpdate;
     }
 
     /**
@@ -63,7 +80,10 @@ public class DebrisCollection implements Updatable {
      */
     private void addDebris(){
         try {
+            //TODO this will create a new debris object to put into the list.
+            //check to see if it has a match in the old list.
             newDebris.addDebris(new DebrisRecord());
+            //check to see if this is a new debris or not.
         }
         catch(NullPointerException e){
             System.err.println("New Debris list not initialized: " + e.getMessage());
@@ -74,7 +94,7 @@ public class DebrisCollection implements Updatable {
      * Get a DebrisRecord from the old list for sending.
      * @return Next DebrisRecord in the old debris list.
      */
-    public DebrisRecord getDebris(){
+    private DebrisRecord getDebris(){
         return oldDebris.getDebrisElement();
     }
 
@@ -98,6 +118,15 @@ public class DebrisCollection implements Updatable {
     private void swapLists(){
         oldDebris = newDebris;
         newDebris = new DRList();
+    }
+
+    /**
+     * Add an update to the outgoingUpdates queue. This will be returned to the
+     * scheduler when the DebrisCollection is polled.
+     * @param u Update that is ready to be returned to the scheduler.
+     */
+    private void queueUpdate(Update u){
+        outgoingUpdates.addLast(u);
     }
 
     /**
