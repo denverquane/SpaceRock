6a3435b82ae81e715b39be7faedffa5c202afcb8
Ken Kressin <jemezgeek.gmail.com>
2017-03-14 21:12:28 -0600

    Added code to make the Take Image case work.

--- src/main/java/fpga/kenTestPackage/FPGAThread.java
+++ src/main/java/fpga/kenTestPackage/FPGAThread.java
@@ -1,6 +1,7 @@
 package fpga.kenTestPackage;
 
 import fpga.memory.MemoryMap;
+import sensor.SensorInterface;
 import sensor.SensorSimulation;
 
 /**
@@ -15,15 +16,43 @@ public class FPGAThread implements Runnable {
   Thread fpgaThread;
   static MemoryMap mm = new MemoryMap();
   static SensorSimulation sensor = new SensorSimulation();
+  static SensorInterface senseI;
   private boolean running = true;
-  FPGAFlags flag;
+  //for TakeImage Flag
+  private boolean RegisterNotReady = true;
+  private long sleepAmount = 500;
+  private FPGAFlags flag;
 
   /**
    * This method gives the ability to shut the thread down in an oredly manner, by setting the
    * boolean variable 'running' to false so the while block will terminate.
    */
-  public void shutdown() {
+  void shutdown() {
     running = false;
+  }
+
+  /**\
+   * This method is used by TakeImage Flag to tell the sensor to take a picture
+   */
+
+  private void SetTakePicture(){
+    while(!sensor.ready()){
+
+    }
+    sensor.takePicture();
+
+    /* Wait for the image to be processed. */
+    while(!sensor.imageReady()){
+
+    }
+  }
+
+  /**
+   * SET SENSOR
+   * Choose which sensor to send instructions to to avoid problems with the constructor.
+   */
+  public void setSensor(SensorInterface si){
+    senseI = si;
   }
 
   /**
@@ -31,7 +60,7 @@ public class FPGAThread implements Runnable {
    *
    * @param name is a String used to name the thread.
    * @param inputFlag is an enum which will be used in the switch block to determine the thread's
-   * behavior.  This allows ond master thread class to create a thread with specific behaviors
+   * behavior.  This allows one master thread class to create a thread with specific behaviors
    * depending on the FPGA flag it is emulating.
    */
   FPGAThread(String name, FPGAFlags inputFlag) {
@@ -85,6 +114,22 @@ public class FPGAThread implements Runnable {
            * to this flag.  I think we can add individual methods needed by the flag out of the
            * switch block.
            */
+          while(RegisterNotReady){
+            RegisterNotReady = true;
+            try{
+              MemoryMap.read(Boolean.class, "take_picture");
+              RegisterNotReady = false;
+            }catch(Exception e){
+              try{
+                //sleep before next poll of register
+                Thread.sleep(sleepAmount);
+              }catch (InterruptedException el){
+                el.printStackTrace();
+              }
+            }
+          }
+          //Have sensor take picture and wait until image processed.
+          SetTakePicture();
 
         }
         break;
