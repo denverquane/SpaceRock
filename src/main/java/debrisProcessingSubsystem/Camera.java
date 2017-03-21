4743850fd7562a103b2170d7b2f8582f2cf4a0cc
Sean Naegle <sean@naegles.com>
2017-03-21 09:32:17 -0600

    added a state enum and change the Camera interface to a class that implements Updatable

--- src/main/java/debrisProcessingSubsystem/Camera.java
+++ src/main/java/debrisProcessingSubsystem/Camera.java
@@ -8,7 +8,30 @@ package debrisProcessingSubsystem;
  * Created by dsr on 3/4/17.
  */
 
-public interface Camera
-{
-  
+public class Camera implements Updatable {
+
+  public Camera() {
+
+  }
+  public void on() {
+
+  }
+
+  public void off() {
+
+  }
+
+  public void takePicture() {
+
+  }
+
+  @Override
+  public Update updateComponent(Update theUpdate) {
+    return null;
+  }
+
+  @Override
+  public Update pollComponent() {
+    return null;
+  }
 }
